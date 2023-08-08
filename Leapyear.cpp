10158d3b2e0cc110f355ba94fa94d1266534b354
Abhishek singh <113301186+Abhisheksingh9368@users.noreply.github.com>
2023-08-08 07:32:08 +0530

    Add files via upload

---
+++ Leapyear.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int y;
+ cout<<"Enter the year =";
+ cin>>y;
+ if(y%4==0){
+    cout<<"This is a leap year";
+ }
+ else{
+    cout<<"THis is not a leap year";
+ }
+
+}
