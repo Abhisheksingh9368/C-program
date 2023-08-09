baa13babdc829051769af1f759693a32f7ee0f0d
Abhishek singh <113301186+Abhisheksingh9368@users.noreply.github.com>
2023-08-09 07:06:57 +0530

    Add files via upload

---
+++ Pattern.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int r;
+    int i;
+    int j;
+    cout<<"Enter how many rows you want =";
+    cin>>r;
+    for(i=0;i<=r;i++){
+        for(j=0;j<=r;j++){
+            cout<<"*";
+        }
+    }
+
+}
+
---
+++ break.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+int i;
+cout<<"Enter the number=";
+cin>>n;
+for(i=0;i<n;i++){
+    if(i==3){
+        cout<<"i understand break";
+        break;
+    }
+    else{
+        cout<<"no is",i;
+    }
+    cout<<"out of the loop";
+}
+
+}
