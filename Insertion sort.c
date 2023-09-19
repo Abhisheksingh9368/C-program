e3568f9bf7dde6a4e13babbfa8a6d0dab3e96156
Abhishek singh <113301186+Abhisheksingh9368@users.noreply.github.com>
2023-09-19 16:29:39 +0530

    Add files via upload

---
+++ Insertion sort.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+
+int i, j,n,key,arr[20];
+printf("Enter number of elements to use:"); scanf("%d",&n);
+printf("Enter %d elements: ",n);
+
+scanf("%d",&arr[i]);
+
+
+j=i-1;
+while((key<arr[j])&&(j>=0)){
+arr[j+1]=arr[j]; j=j-1; }
+arr[j+1]=key;
+}
+
+
+
+
+
+printf("Sorted elements: "); for(i=0;i<n;i++) printf(" %d",arr[i]); getch();
+}
